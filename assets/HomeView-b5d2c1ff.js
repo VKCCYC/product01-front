b3e4f2c8dd30940d9f3831cdb29a9a9404e86fd3
VKCCYC <145089052+VKCCYC@users.noreply.github.com>
2024-02-22 14:16:35 +0000

    Deploying to gh-pages from @ VKCCYC/product01-front@63ef1e89bf32818a4e1ad6f1465da919c7bcf9c6 🚀

--- assets/HomeView-b5d2c1ff.js
+++
@@ -1,1 +0,0 @@
-import{C as e,D as o,_ as c}from"./index-4c6fc963.js";function n(t,a){return e(),o("h1",null,"home")}const r={},_=c(r,[["render",n]]);export{_ as default};
--- assets/ssrBoot-1ca997ca.js
+++
@@ -1,1 +0,0 @@
-import{ab as t,Y as e,z as s,b5 as n}from"./index-4c6fc963.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
